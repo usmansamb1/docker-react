2e16ff52f087125cb1ef11481e74eaa18e7dbd11
usmansamb1 <usmansamb@gmail.com>
2021-03-16 12:56:40 +0500

    initial comments

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          44444 what is that<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          what is champ 33 think
+ 
         </a>
       </header>
     </div>
