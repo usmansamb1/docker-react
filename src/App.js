15e8c854415b5ee7390d9626235e0ab077e182ba
usmansamb1 <usmansamb@gmail.com>
2021-03-16 14:36:30 +0500

    adding travis and correct some text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          44444 what is that<code>src/App.js</code> and save to reload.
+        learn react that<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
